940f3cc056e9c8ac56904cedcacba9ebd552e9d9
tekaCM <142100857+tekaCM@users.noreply.github.com>
2023-08-13 15:44:02 +0200

    Add files via upload

---
+++ Worker.js
@@ -0,0 +1,42 @@
+const ConfigManager = require("./Config");
+const config = new ConfigManager("./secret/Data.json");
+
+class Server {
+    constructor() {
+        this.mainUrl = "https://launcher.gvmp.de:5004";
+    }
+
+    loginPlayer(username, password) {
+        const payload = {
+            username: username,
+            password: sha256(password)
+        }
+
+        return axios.post(`${this.mainUrl}/auth/login`, payload)
+                .then(response => response.data)
+    }
+
+    whitelistPlayer() {
+        const payload = {
+            auth_token: config.read("auth_token"),
+            name: config.read("username"),
+            proxy: config.read("proxy"),
+            forum_id: config.read("forum_id")
+        }
+
+        return axios.post(`${this.mainUrl}/player/whitelist`, payload)
+                .then(response => response.data)
+    }
+
+    isPlayerOnline(id) {
+        return axios.get(`${this.mainUrl}/player/online/${id}`)
+                .then(response => response.data)    
+    }
+
+    isPlayerWhitelisted(id) {
+        return axios.get(`${this.mainUrl}/whitelist/ip/validate/${id}`)
+                .then(response => response.data)  
+    }
+}
+
+exports.Server = Server;
